f4808c247b2347f723064d8edb0ac112138e2b8d
Honlan <honlan@192.168.0.106>
2016-07-31 13:20:27 +0800

    prepare for ppt

--- codes/R/air.R
+++ codes/R/air.R
@@ -8,13 +8,13 @@ map_line <- readShapePoly("maps/bou4/BOUNT_poly.shp")
 ShandongJiangsu <- fortify(map_line[substr(as.character(map_line$ADCODE99),1,2) == '37' | substr(as.character(map_line$ADCODE99),1,2) == '32',])
 names(ShandongJiangsu)[1:2] <- c("经度", "纬度")
 # 空气监测站点分布
-ggplot() + geom_polygon(data=ShandongJiangsu, aes(x=经度, y=纬度, group=id), color="grey", fill=NA) + theme_grey() + coord_map() + geom_point(data=qingyue_air_station, aes(x=经度,y=纬度,color=城市,shape=省份)) + labs(title='空气监测站点地理分布') + theme(text=element_text(family="Microsoft YaHei"), legend.title=element_blank())
-ggplot() + geom_bar(data=qingyue_air_station, aes(x=城市, fill=省份), stat="count") + labs(y='空气监测站点数量') + theme(text=element_text(family="Microsoft YaHei"), legend.title=element_blank(), axis.text.x=element_text(angle=90))
+ggplot() + geom_polygon(data=ShandongJiangsu, aes(x=经度, y=纬度, group=id), color="grey", fill=NA) + theme_grey() + coord_map() + geom_point(data=qingyue_air_station, aes(x=经度,y=纬度,color=城市,shape=省份)) + labs(title='空气_1_山东江苏空气监测站点地理分布') + theme(text=element_text(family="Microsoft YaHei"), legend.title=element_blank())
+ggplot() + geom_bar(data=qingyue_air_station, aes(x=城市, fill=省份), stat="count") + labs(y='空气监测站点数量', title='空气_2_山东江苏空气监测站点城市统计') + theme(text=element_text(family="Microsoft YaHei"), legend.title=element_blank(), axis.text.x=element_text(angle=90))
 
 # AQI历史数据折线图
 qingyue_air_data <- qingyue_air_data[qingyue_air_data$省份!='',]
 qingyue_air_data$发布时间 <- as.POSIXct(qingyue_air_data$发布时间, format="%Y-%m-%d %H:%M:%S")
-ggplot(data=qingyue_air_data) + geom_line(aes(x=发布时间, y=AQI)) + theme_grey() + facet_grid(省份~.) + theme(text=element_text(family="Microsoft YaHei"), legend.title=element_blank())
+ggplot(data=qingyue_air_data) + geom_line(aes(x=发布时间, y=AQI)) + theme_grey() + facet_grid(省份~.) + labs(title='空气_3_山东江苏历史AQI数据') + theme(text=element_text(family="Microsoft YaHei"), legend.title=element_blank())
 # AQI历史数据一维密度图和二维密度图
-ggplot(data=qingyue_air_data) + geom_density(aes(x=AQI)) + theme_grey() + facet_grid(省份~.) + theme(text=element_text(family="Microsoft YaHei"), legend.title=element_blank())
-ggplot(data=qingyue_air_data, aes(x=发布时间, y=AQI)) + stat_density2d(aes(fill = ..level..), geom="polygon") + scale_fill_continuous(high='darkblue',low='white') + facet_grid(省份~.) + theme(text=element_text(family="Microsoft YaHei"), legend.title=element_blank())
+ggplot(data=qingyue_air_data) + geom_density(aes(x=AQI)) + theme_grey() + facet_grid(省份~.) + labs(y='分布密度', title='空气_4_山东江苏历史AQI一维密度图') + theme(text=element_text(family="Microsoft YaHei"), legend.title=element_blank())
+ggplot(data=qingyue_air_data, aes(x=发布时间, y=AQI)) + labs(y='分布密度', title='空气_5_山东江苏历史AQI二维密度图') + stat_density2d(aes(fill = ..level..), geom="polygon") + scale_fill_continuous(high='darkblue',low='white') + facet_grid(省份~.) + theme(text=element_text(family="Microsoft YaHei"), legend.title=element_blank())
--- codes/R/pollution.R
+++ codes/R/pollution.R
@@ -8,4 +8,5 @@ map_line <- readShapePoly("maps/bou4/BOUNT_poly.shp")
 Shandong <- fortify(map_line[substr(as.character(map_line$ADCODE99),1,2) == '37',])
 names(Shandong)[1:2] <- c("经度", "纬度")
 qingyue_pollution_company <- qingyue_pollution_company[qingyue_pollution_company$经度 >=114.8 & qingyue_pollution_company$经度 <= 122.7 & qingyue_pollution_company$纬度 >= 34.39 & qingyue_pollution_company$纬度 <= 38.41, ]
-ggplot() + geom_polygon(data=Shandong, aes(x=经度, y=纬度, group=id), color="grey", fill=NA) + theme_grey() + coord_map() + geom_point(data=qingyue_pollution_company, aes(x=经度,y=纬度,color=市)) + labs(title='污染企业地理分布') + theme(text=element_text(family="Microsoft YaHei"), legend.title=element_blank())
+ggplot() + geom_polygon(data=Shandong, aes(x=经度, y=纬度, group=id), color="grey", fill=NA) + labs(title='污染_1_山东污染企业地理分布') + theme_grey() + coord_map() + geom_point(data=qingyue_pollution_company, aes(x=经度,y=纬度,color=市)) + labs(title='污染企业地理分布') + theme(text=element_text(family="Microsoft YaHei"), legend.title=element_blank())
+ggplot() + geom_bar(data=qingyue_pollution_company, aes(x=市), stat="count") + labs(y='污染企业数量', title='污染_2_山东污染企业城市统计') + theme(text=element_text(family="Microsoft YaHei"), legend.title=element_blank(), axis.text.x=element_text(angle=90))
